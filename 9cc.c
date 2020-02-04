21818d9f8e7dd96d25dc9107fc6988910c64470d
Kazuki Shigemichi <shigemichik@gmail.com>
2020-02-04 23:03:44 +0900

    パーサを書き足した

--- 9cc.c
+++ 9cc.c
@@ -125,6 +125,81 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+// 抽象構文木のノード種別
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 数値
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind; // ノードの種類
+    Node *lhs;     // 左辺
+    Node *rhs;     // 右辺
+    int val;       // kindがND_NUMのときに使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *primary(void);
+Node *mul(void);
+Node *expr(void);
+
+Node *primary(void) {
+    // 括弧で囲まれている場合
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    // そうでなければ数値でなければならない
+    return new_node_num(expect_number());
+}
+
+Node *mul(void) {
+    Node *node = primary();
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if(consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *expr(void) {
+    Node *node = mul();
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
