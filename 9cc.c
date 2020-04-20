9a12f6a6bef9f585674afa9e6025c2af9ca73186
Kazuki Shigemichi <shigemichik@gmail.com>
2020-04-20 14:09:47 +0900

    単項演算子が連続する場合に対応させた
    リファレンス実装に合わせて変更

--- 9cc.c
+++ 9cc.c
@@ -205,10 +205,10 @@ Node *expr(void) {
 
 Node *unary(void) {
     if (consume('+')) {
-        return primary();
+        return unary();
     }
     if (consume('-')) {
-        return new_node(ND_SUB, new_node_num(0), primary());
+        return new_node(ND_SUB, new_node_num(0), unary());
     }
     return primary();
 }
